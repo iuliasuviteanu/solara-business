b112ad1d7e186ae9f852ba0cc4b60627e75a0f35
iulia.suviteanu <iulia.suviteanu@softvision.com>
2023-09-05 11:21:35 +0300

    strapi vercel updates

--- solara-strapi/config/env/production/plugins.js
+++ solara-strapi/config/env/production/plugins.js
@@ -1,15 +1,5 @@
-module.exports = {
-  graphql: {
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 10,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-        introspection: true,
-      },
-    },
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
   },
-};
+});
